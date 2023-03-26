c0ea66ea302f8bbc4aa60bce4b047f0a8e40b73d
aizaz <i200406@nu.edu.pk>
2023-03-27 03:11:15 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbarr from "./Navbar";
+import ItemCards from "./ItemCards";
+import ArticlePage from "./ArticlePage";
+import Cart from "./Cart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+
+    <Navbarr/>
+
+      <Routes>
+        <Route path="/" element={<ItemCards/>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/ArticlePage/:title" element={<ArticlePage/>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+        <Route path="/:title" element={<ItemCards/>} />
+        <Route path="/cart" element={<Cart/>} />
+      </Routes>
+    </Router>
+
+
+
+    );
 }
 
 export default App;
